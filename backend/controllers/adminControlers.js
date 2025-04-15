5c3d9ec097c794a98f0e47789bc6415556d0cb7b
Rahul <kasurirahul6@gmail.com>
2025-04-15 13:54:17 +0530

    pushing

---
+++ backend/controllers/adminControlers.js
@@ -0,0 +1,11 @@
+const adddoctor = async()=>{
+
+    try{
+        const{name,email,password,speciality,degree,experience,about,fees,address} = req.body
+        
+
+    }
+    catch(error){
+
+    }
+}
